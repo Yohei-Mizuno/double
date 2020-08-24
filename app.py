213e72d1687e2c09701b701f74d7bf92aed29f6a
Yohei-Mizuno <mizyotama2525@gmail.com>
2020-08-24 20:21:54 +0900

    17行目で()を入れるとstrの割り振り番号のみprintされる 実行の()をいれるとTypeErrorが発生

---
+++ app.py
@@ -0,0 +1,18 @@
+def double(num):
+    return num * 2
+
+
+print(double(1))
+
+
+#def str_combine(name, family_name):
+#   return name + ' ' + family_name
+
+def str_combine(name, family_name):
+    return f'{name}{family_name}'
+
+
+str_combine('Yohei', "Mizuno")
+
+print(str_combine())
+#1
